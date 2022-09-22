eef3de28a56788503b838822e194173d1467f6a9
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-22 19:26:00 +0300

    first commit

---
+++ src/sayHello.test.js
@@ -0,0 +1,5 @@
+import sayHello from "./sayHello";
+
+test("deneme", () => { 
+    expect(sayHello("merhaba")).toBe("MERHABA")
+ })
---
+++ src/sum.js
@@ -0,0 +1,5 @@
+const sum = (s1, s2) => {
+    return s1+s2
+}
+
+export default sum;
